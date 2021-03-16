a219b48e3cbd28f7f018743aa435f3b6340b2e2a
Arsentiy Zhunussov <asekenskigo@gmail.com>
2021-03-16 20:28:14 +0600

    Corrected update method in Batch, returns id

--- app/Models/Acquisition/Batch/BatchManage.php
+++ app/Models/Acquisition/Batch/BatchManage.php
@@ -5,9 +5,21 @@ namespace App\Models\Acquisition\Batch;
 
 
 use App\Common\Helpers\Query\ManageBuilder;
+use Illuminate\Support\Facades\DB;
 
 trait BatchManage
 {
+    public static function create(array $attributes): object
+    {
+        DB::beginTransaction();
+
+        $data = (new static)->fill($attributes);
+        $data->save();
+
+        DB::commit();
+        return $data;
+    }
+
     public function update(array $attributes = [], array $options = []): bool
     {
         return ManageBuilder::updateBuilder(self::class, $this->getKeyName(), $this->hesab_id, $attributes, $this->fillable);
