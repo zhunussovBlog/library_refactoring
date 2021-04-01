7815ebbebc0ea0c628112f0805d7760c172c7694
Arsentiy Zhunussov <asekenskigo@gmail.com>
2021-04-01 10:50:15 +0600

    Some addings

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Common\Helpers\Controller\AuthProcedure;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserLoginRequest;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
@@ -24,9 +25,13 @@ class LoginController extends Controller
 
         session()->put('user', $user);
 
+        $token = $user->createToken(config('auth.token'));
+
         $user = Auth::user();
 
         return response()->json(['res' => [
+            'access_token' => $token->accessToken,
+            'expires_in' => $token->token->expires_at->diffInSeconds(Carbon::now()),
             'user' => $user,
         ]]);
     }
