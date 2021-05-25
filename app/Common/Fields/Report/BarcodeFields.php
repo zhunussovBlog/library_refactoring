a853e657818aed8f17434402c75823b22c362215
Arsentiy Zhunussov <asekenskigo@gmail.com>
2021-05-25 23:46:51 +0600

    Search by barcode (like) added in print barcodes

--- app/Common/Fields/Report/BarcodeFields.php
+++ app/Common/Fields/Report/BarcodeFields.php
@@ -12,6 +12,7 @@ class BarcodeFields extends FieldInterface
     protected static array $searchFields = [
         ['key' => 'barcode', 'title' => 'By barcode', 'method' => SearchFieldConstants::RANGE_NUMBER],
         ['key' => 'author', 'title' => 'Author', 'method' => SearchFieldConstants::LIKE],
-        ['key' => 'title', 'title' => 'Title', 'method' => SearchFieldConstants::LIKE]
+        ['key' => 'title', 'title' => 'Title', 'method' => SearchFieldConstants::LIKE],
+        ['key' => 'barcode_like', 'title' => 'Barcode', 'method' => SearchFieldConstants::LIKE],
     ];
 }
